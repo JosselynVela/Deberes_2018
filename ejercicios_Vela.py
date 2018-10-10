25d4932d6cd3c6c5b130cd7650db0e6f43bb8f7c
JosselynVela <43852960+JosselynVela@users.noreply.github.com>
2018-10-10 18:27:04 -0500

    Add files via upload

---
+++ ejercicios_Vela.py
@@ -0,0 +1,46 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2-2
+0
+>>> 3+6
+9
+>>> 4+2
+6
+>>> 3**3
+27
+>>> 8*84
+672
+>>> 4/4
+1.0
+>>> 'hola'+'mundo'
+'holamundo'
+>>> 'hola' + '+mundo'
+'hola+mundo'
+>>> 'jos' *3 + vela
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    'jos' *3 + vela
+NameError: name 'vela' is not defined
+>>> 'jos'*3+'vela'
+'josjosjosvela'
+>>> a= 'programacion'
+>>> print(a[1])
+r
+>>> print(a[-2])
+o
+>>> a='hola'
+>>> b='que hace'
+>>> c=''
+>>> print (a+b+c)
+holaque hace
+>>> print(a+c+b+c)
+holaque hace
+>>> c=' '
+>>> print(a+c+b+c)
+hola que hace 
+>>> type(a)
+<class 'str'>
+>>> x=int(25)
+>>> type(x)
+<class 'int'>
+>>> 
